00b9910c1be3d67c11b55c62fe32a31282032ba7
Daniel Marques <daniel.silva.city@gmail.com>
2023-07-20 23:41:47 -0300

    refactor: separa campos não opcionais em props com interface

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,15 +1,21 @@
 import { randomUUID } from 'node:crypto'
 
+interface AnswerProps {
+  content: string
+  authroId: string
+  questionId: string
+}
+
 export class Answer {
   public id?: string
   public content: string
   public authorId: string
   public questionId: string
 
-  constructor(content: string, authroId: string, questionId: string, id?: string) {
-    this.content = content
-    this.authorId = authroId
-    this.questionId = questionId
+  constructor(props: AnswerProps, id?: string) {
+    this.content = props.content
+    this.authorId = props.authroId
+    this.questionId = props.questionId
     this.id = id ?? randomUUID()
   }
 }
