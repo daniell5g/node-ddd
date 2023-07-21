b62596041f48c70e22bcdde9a483b2bfb843f10a
Daniel Marques <daniel.silva.city@gmail.com>
2023-07-20 23:09:58 -0300

    feat: adiciona identificadores das entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,13 @@
+import { randomUUID } from 'node:crypto'
+
 export class Question {
+  public id?: string
   public title: string
   public description: string
 
-  constructor(title: string, description: string) {
+  constructor(title: string, description: string, id?: string) {
     this.title = title
     this.description = description
+    this.id = id ?? randomUUID()
   }
 }
