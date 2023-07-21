dce9fc4023474cc66f8c7d826b164c0833d81e91
Daniel Marques <daniel.silva.city@gmail.com>
2023-07-20 23:19:07 -0300

    feat: implementa caso de Answer question

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,10 +1,14 @@
+import { Answer } from './../entities/answer';
 interface AnswerQuestionUseCaseRequest {
   instructorId: string;
   questionId: string;
+  content: string;
 }
 
 export class AnswerQuestionCase {
-  execute({ instructorId, questionId }: AnswerQuestionUseCaseRequest) {
-    // ...
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer(content)
+
+    return answer
   }
 }
