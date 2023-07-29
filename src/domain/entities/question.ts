29a2fbc7edbfaa4154f3c31772f76522bca060f8
Daniel Marques <daniel.silva.city@gmail.com>
2023-07-29 00:49:22 -0300

    refactor: refatora entidade de questão

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,16 +1,18 @@
-import { randomUUID } from 'node:crypto'
 import { Slug } from './values-objects/slug'
+
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
-  slug: Slug
   content: string
-  authorId: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
-  constructor(props: QuestionProps, id?: string) {
-    super(props, id)
-  }
+
 }
